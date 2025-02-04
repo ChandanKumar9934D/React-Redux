d9c2c5291913df48d9b1eb18e6bd589de13a71e4
Chandan Kumar <chandankumarchandankumar38851@gmail.com>
2025-02-04 23:30:38 +0530

    learn react again frome strat first project is start is Todo

---
+++ react$redux/01project/vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import Button from "./myButton"
+
+function App() {
+
+  return (
+  <>
+  <Button/>
+  </>
+  )
+}
+
+export default App
---
+++ react$redux/01project/vite-project/src/myButton.jsx
@@ -0,0 +1,6 @@
+export default function Button() {
+    return(
+        <h1>its my first button</h1>
+    )
+    
+}

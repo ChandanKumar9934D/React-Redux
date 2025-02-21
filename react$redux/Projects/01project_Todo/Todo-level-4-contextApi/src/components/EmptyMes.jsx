2fa566fade1cd29cc7e6e04ba53d6c6393791f19
Chandan Kumar <chandankumarchandankumar38851@gmail.com>
2025-02-21 13:49:34 +0530

    learn useRfe hook and create next lavel of todo project in this project learn context API

---
+++ react$redux/Projects/01project_Todo/Todo-level-4-contextApi/src/components/EmptyMes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const EmptyMes = () => {
+  return (
+    <>
+    <h3 className='text-center mt-2 text-capitalize'>happy food and helthy food</h3>
+      
+    </>
+  )
+}
+
+export default EmptyMes

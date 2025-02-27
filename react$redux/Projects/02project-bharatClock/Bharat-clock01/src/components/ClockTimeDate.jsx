1f6ecc9bbdddcf687cd0c3ab52aa01f32490146c
Chandan Kumar <chandankumarchandankumar38851@gmail.com>
2025-02-27 22:36:50 +0530

    update clock project

--- react$redux/Projects/02project-bharatClock/Bharat-clock01/src/components/ClockTimeDate.jsx
+++ react$redux/Projects/02project-bharatClock/Bharat-clock01/src/components/ClockTimeDate.jsx
@@ -1,20 +1,31 @@
-import React from 'react'
-import "../App.css"
+import React, { useEffect, useState } from "react";
+import "../App.css";
 const ClockTimeDate = () => {
-    let date=new Date()
-    return (
-        <>
-            <div className="container">
-                <div className="row">
-                    <div className="col-sm-12">
-                        <p className="slog lead fw-normal fs-3">This is the currunt time : {date.toLocaleDateString()}- {date.toLocaleTimeString()}</p>
+  // let date=new Date()
 
-                    </div>
-                </div>
-            </div>
+  const [date, setNewDate] = useState(new Date());
+  useEffect(() => {
+    let setIntervalId = setInterval(() => {
+      setNewDate(new Date());
+    }, 1000);
+    return () => {
+      clearInterval(setIntervalId);
+    };
+  }, []);
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12">
+            <p className="slog lead fw-normal fs-3">
+              This is the currunt time : {date.toLocaleDateString()}-{" "}
+              {date.toLocaleTimeString()}
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-        </>
-    )
-}
-
-export default ClockTimeDate
+export default ClockTimeDate;
